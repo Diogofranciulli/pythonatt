e43ed23f3284ab09b437ffe5a9e2237ae28eac8f
Diogo Leles Franciulli <54507748+Diogofranciulli@users.noreply.github.com>
2024-08-29 22:46:02 -0300

    exercicios

---
+++ exercicios/exercicio2.py
@@ -0,0 +1,10 @@
+a = []
+
+for i in range():
+    simOuNao = input("digite o valor// S para continuar e N para parar")
+    if simOuNao == "s":
+        a.append(simOuNao)
+        continue
+    elif simOuNao == "n":
+        break
+
