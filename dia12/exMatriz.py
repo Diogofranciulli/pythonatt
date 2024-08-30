92312c0318a3a1957f79146e8bdd82ec6398139c
Laboratório FIAP <labsfiap@fiap.com.br>
2024-08-30 19:56:04 -0300

    exerc

--- dia12/exMatriz.py
+++ dia12/exMatriz.py
@@ -1,10 +1,12 @@
 valor = int(input("digite o valor para x"))
 def funcao(valor):
 
-    if valor >= 0:
+    if valor >= 0 and valor < 5:
         print(f"O valor de y é {valor}")
     elif valor < 0:
         print(f"o valor de y é {valor ** 2}")
+    elif valor > 5:
+        print(f"o valor de y é {6}")
     else:
         print("erro")
 
