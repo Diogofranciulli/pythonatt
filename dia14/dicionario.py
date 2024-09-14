36071d4aa0ae00357ca37d6c8d9761a94a332a34
Laboratório FIAP <labsfiap@fiap.com.br>
2024-09-13 22:28:36 -0300

    exercicios dicionario

---
+++ dia14/dicionario.py
@@ -0,0 +1,13 @@
+dicionarioA = {
+    'nome': 'diogo',
+    'idade': '18',
+    1 : 'oi',
+    1.23: 37
+}
+
+dicionarioB = {
+    "propriedade": "Janela",
+    "Ar": True
+}
+dicionarioA.update(dicionarioB)
+print(dicionarioA)
--- dia14/xadrez.py
+++ dia14/xadrez.py
@@ -12,8 +12,14 @@ xadrez = [
 
 ]
 
-plt.imshow(xadrez)
+# plt.imshow(xadrez)
+# plt.axis("off")
+# plt.show()
+linha = int(input(":"))
+coluna = int(input(":"))
+xadrez1 = [[[255*((i+j)%2),255*((i+j)%2),255*((i+j)%2)] for i in range (coluna)] for j in range(linha)]
+print(xadrez1)
+
+plt.imshow(xadrez1)
 plt.axis("off")
 plt.show()
-
-
