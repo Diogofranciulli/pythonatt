69750fc5ba840635bf2c755c5c2d905ea8c595b4
Laboratório FIAP <labsfiap@fiap.com.br>
2024-09-27 22:43:29 -0300

    bubble sort

--- dia16/bubble_sort.py
+++ dia16/bubble_sort.py
@@ -2,8 +2,8 @@ a = [7,4,9,1,10,1]
 print(a)
 
 
-#for j in range(len(a)):
-    for i in range(len(a)-1):
+for j in range(len(a)-1,1,-1):
+    for i in range(j):
         if a[i] > a[i+1]:
             aux = a[i]
             a[i] = a[i + 1]
